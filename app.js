50d7a2c819148ef04a470040029f955b2726a710
unknown <JNKarif>
2023-12-09 16:05:34 +0600

    cross element added

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ function addTask() {
   if (inputBox.value === "") {
     alert("Please add a task!");
   } else {
+    /**create new li inside ul by js: 
+    step 1: createElement li ,\
+    step 2: insert text from inputbox into li
+    stet 3: now append all of this new li to ul by listContainer id */
     let list = document.createElement("li");
     list.innerHTML = inputBox.value;
     listContainer.appendChild(list);
+    // Adding a cross icon
+    let cross = document.createElement("cross");
+    cross.innerHTML = "\u00d7"; //for cross icon code: \u00d7
+    list.appendChild(cross);
   }
+  inputBox.value = ""; //to empty the input box after adding task
 }
