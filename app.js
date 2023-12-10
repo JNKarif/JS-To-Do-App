95231ceb7b88323b72d2025f21089afb08028e16
unknown <JNKarif>
2023-12-10 13:10:23 +0600

    final coding completed

--- app.js
+++ app.js
@@ -13,9 +13,55 @@ function addTask() {
     list.innerHTML = inputBox.value;
     listContainer.appendChild(list);
     // Adding a cross icon
-    let cross = document.createElement("cross");
+    let cross = document.createElement("span");
     cross.innerHTML = "\u00d7"; //for cross icon code: \u00d7
     list.appendChild(cross);
   }
   inputBox.value = ""; //to empty the input box after adding task
+  saveData();
 }
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    /** To cut and mark as checked the selected line:
+     * step 1: in the css text-decoration: line-through to cut the line horizental line;
+     * step 2: use upper case for tag name
+     * step 3: use this function with if else, toggle(), remove()*/
+    if (e.target.tagName === "LI") {
+      e.target.classList.toggle("checked");
+      saveData();
+    } else if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveData();
+    }
+  },
+  false
+);
+
+function saveData() {
+  localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+  const savedData = localStorage.getItem("data");
+
+  if (savedData) {
+    listContainer.innerHTML = savedData;
+  }
+}
+
+showTask();
+
+/**
+ * searchBox.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    checkWeather(searchBox.value);
+  }
+ */
+
+inputBox.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {
+    addTask(inputBox.value);
+  }
+});
